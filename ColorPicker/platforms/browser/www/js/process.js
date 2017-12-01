c9557ca12fbe134c933f7904c72fcae21309875a
svictorio <sanjevictorio@gmail.com>
2017-12-01 19:25:46 +0800

    ColorPicker

---
+++ ColorPicker/platforms/browser/www/js/process.js
@@ -0,0 +1,46 @@
+function Cwhite() {
+  var white = document.getElementById("white");
+  if (white.paused) {
+    white.play();
+  } else {
+    white.pause();
+  }
+}
+
+function Cblue() {
+  var blue = document.getElementById("blue");
+  if (blue.paused) {
+    blue.play();
+  } else {
+    blue.pause();
+  }
+}
+
+function Cgreen() {
+  var green = document.getElementById("green");
+  if (green.paused) {
+    green.play();
+  } else {
+    green.pause();
+  }
+}
+
+
+function Corange() {
+  var orange = document.getElementById("orange");
+  if (orange.paused) {
+    orange.play();
+  } else {
+    orange.pause();
+  }
+}
+
+
+function Cred() {
+  var red = document.getElementById("red");
+  if (red.paused) {
+    red.play();
+  } else {
+    red.pause();
+  }
+}
---
+++ ColorPicker/www/js/audio.js
@@ -0,0 +1,10 @@
+
+var cherry = document.getElementById('s');
+
+var dom = 
+
+	<div>
+		<audio src="audio/CHERRY.mp3"/>
+	</div>
+
+ReactDOM.render(dom,cherry);
