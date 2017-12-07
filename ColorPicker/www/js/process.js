c573f8d16e4fd38f2f282156afb3390adf70862f
Svictorio15 <30882416+svictorio@users.noreply.github.com>
2017-12-07 18:07:56 +0800

    Update process.js

--- ColorPicker/www/js/process.js
+++ ColorPicker/www/js/process.js
@@ -1,151 +1,94 @@
-// <video width="320" height="240" controls>
-//   <source src="movie.mp4" type="video/mp4">
-//   <source src="movie.ogg" type="video/ogg">
-//   Your browser does not support the video tag.
-// </video>
-
-// let process = {
-// 	menu:function(){
-// 		let source_vid = 'https://www.youtube.com/watch?v=d-P7YUSKbpQ';
-// 		let content = <div>
-// 		    <iframe src={source_vid} width="100%" height='300px'/>
-// 			<audio id="audio_bomb">
-// 				<source src="audio/bomb.mp3" type="audio/mp3" />
-// 			</audio>
-// 			<ul>
-// 				<li><a href='#' className='imCustomClass' onClick={process.page1}>Menu 1</a></li>
-// 				<li><a href='#' onClick={process.page2}>Menu 2</a></li>
-// 				<li><a href='#' onClick={process.page3}>Menu 3</a></li>
-// 				<li><a href='#' onClick={process.page4}>Menu 4</a></li>
-// 			</ul>
-// 		</div>;
-// 		ReactDOM.render(content,document.getElementById('root'));
-// 	},
-// 	page1:function(){
-// 		process.audio('audio_bomb').play();
-// 		let content = <div>
-// 				this is page 1<br/><a href='#' onClick={process.menu}>back</a>
-// 			</div>;
-// 			ReactDOM.render(content,document.getElementById('root'));
-// 		process.audio('audio_bomb').pause();
-// 	},
-// 	page2:function(){
-// 		process.audio('audio_bomb').play();
-// 		let content = <div>
-// 				this is page 2<br/><a href='#' onClick={process.menu}>back</a>
-// 			</div>;
-// 			ReactDOM.render(content,document.getElementById('root'));
-// 		process.audio('audio_bomb').pause();
-// 	},
-// 	page3:function(){
-// 		process.audio('audio_bomb').play();
-// 		let content = <div>
-// 				this is page 3<br/><a href='#' onClick={process.menu}>back</a>
-// 			</div>;
-// 			ReactDOM.render(content,document.getElementById('root'));
-// 		process.audio('audio_bomb').pause();
-// 	},
-// 	page4:function(){
-// 		process.audio('audio_bomb').play();
-// 		let content = <div>
-// 				this is page 4<br/><a href='#' onClick={process.menu}>back</a>
-// 			</div>;
-// 			ReactDOM.render(content,document.getElementById('root'));
-// 		process.audio('audio_bomb').pause();
-// 	},
-	// loading:function(){
-	// 	let content = <div>
-	// 				<div className="progressbar-infinite color-multi"></div>
-	// 				<audio id="audio">
-	// 					<source src="audio/horse.ogg" type="audio/ogg" />
-	// 				</audio>
-	// 				<div align='center'>
-	// 					<img src='img/image.jpg' width='100px' />
-	// 					Loading...
-	// 				</div>
-	// 			</div>;
-	// 	ReactDOM.render(content,document.getElementById('root'));
-	// 	process.audio('audio').play();
-	// 	process.replayAudio('audio');
-	// }
-// 	audio:function(audio){
-// 		let _audio = document.getElementById(audio);
-// 		return _audio;
-// 	},
-// 	replayAudio:function(audio){
-// 		process.audio(audio).addEventListener('ended',function(){
-// 			process.audio('audio').pause();
-// 			process.audio(audio).play();
-// 		})
-// 	}
-// }
-
-// let counting = 0;
-// process.loading();
-// setTimeout(function(){
-// 	process.audio('audio').pause();
-// 	process.menu();
-// },5000);
-
-
-function btnRed() {
-  document.getElementById("Div1").style.backgroundColor="Red";
+let process = {
+	menu:function(){
+		let content =<div style={{background:"blue"}}>
+			<center>
+			
+			<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+			<h1>Color Picker</h1>
+			<button><a href='#' onClick={process.Red}>Red</a></button>
+			<button><a href='#' onClick={process.Green}>Green</a></button>
+			<button><a href='#' onClick={process.Orange}>Orange</a></button>
+			<button><a href='#' onClick={process.White}>White</a></button>
+			<button><a href='#' onClick={process.menu}>Blue</a></button><br/>
+			<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+			<div style={{background:"blue"}}>
+			<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>	<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>		<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+</div>
+			</center>
+			</div>
+		ReactDOM.render(content,document.getElementById('root'));
+	},
+Red:function(){
+	let content =<div style={{background:"red"}}>
+			<center>
+			<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+			<h1>Color Picker</h1>
+			<button><a href='#' onClick={process.Red}>Red</a></button>
+			<button><a href='#' onClick={process.Green}>Green</a></button>
+			<button><a href='#' onClick={process.Orange}>Orange</a></button>
+			<button><a href='#' onClick={process.White}>White</a></button>
+			<button><a href='#' onClick={process.menu}>Blue</a></button>
+			<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+						<div style={{background:"red"}}>
+			<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>	<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>		<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+</div>
+			</center>
+			</div>
+		ReactDOM.render(content,document.getElementById('root'));
+},
+Green:function(){
+	let content =<div style={{background:"green"}}>
+			<center>
+			<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+			<h1>Color Picker</h1>
+			<button><a href='#' onClick={process.Red}>Red</a></button>
+			<button><a href='#' onClick={process.Green}>Green</a></button>
+			<button><a href='#' onClick={process.Orange}>Orange</a></button>
+			<button><a href='#' onClick={process.White}>White</a></button>
+			<button><a href='#' onClick={process.menu}>Blue</a></button>
+			<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+						<div style={{background:"green"}}>
+<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>	<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>					<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+</div>
+			</center>
+			</div>
+		ReactDOM.render(content,document.getElementById('root'));
+},
+Orange:function(){
+	let content =<div style={{background:"orange"}}>
+			<center>
+			<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+			<h1>Color Picker</h1>
+			<button><a href='#' onClick={process.Red}>Red</a></button>
+			<button><a href='#' onClick={process.Green}>Green</a></button>
+			<button><a href='#' onClick={process.Orange}>Orange</a></button>
+			<button><a href='#' onClick={process.White}>White</a></button>
+			<button><a href='#' onClick={process.menu}>Blue</a></button>
+			<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+						<div style={{background:"orange"}}>
+<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>	<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>					<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+</div>
+			</center>
+			</div>
+		ReactDOM.render(content,document.getElementById('root'));
+},
+White:function(){
+	let content =<div style={{background:"white"}}>
+			<center>
+			<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+			<h1>Color Picker</h1>
+			<button><a href='#' onClick={process.Red}>Red</a></button>
+			<button><a href='#' onClick={process.Green}>Green</a></button>
+			<button><a href='#' onClick={process.Orange}>Orange</a></button>
+			<button><a href='#' onClick={process.White}>White</a></button>
+			<button><a href='#' onClick={process.menu}>Blue</a></button>
+			<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+						<div style={{background:"white"}}>
+	<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>					<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+</div>
+			</center>
+			</div>
+		ReactDOM.render(content,document.getElementById('root'));
 }
-function btnGreen() {
-  document.getElementById("Div1").style.backgroundColor="Green";
 }
-function btnBlue() {
-  document.getElementById("Div1").style.backgroundColor="Blue";
-}
-function btnWhite() {
-  document.getElementById("Div1").style.backgroundColor="White";
-}
-
-function btnOrange() {
-  document.getElementById("Div1").style.backgroundColor="Orange";
-}
-
-function Cred() {
-  var red = document.getElementById("red");
-  if (red.paused) {
-    red.play();
-  } else {
-    red.pause();
-  }
-}
-
-function Cwhite() {
-  var white = document.getElementById("white");
-  if (white.paused) {
-    white.play();
-  } else {
-    white.pause();
-  }
-}
-
-function Cblue() {
-  var blue = document.getElementById("blue");
-  if (blue.paused) {
-    blue.play();
-  } else {
-    blue.pause();
-  }
-}
-
-function Cgreen() {
-  var green = document.getElementById("green");
-  if (green.paused) {
-    green.play();
-  } else {
-    green.pause();
-  }
-}
-
-function Corange() {
-  var orange = document.getElementById("orange");
-  if (orange.paused) {
-    orange.play();
-  } else {
-    orange.pause();
-  }
-}
+process.menu();
